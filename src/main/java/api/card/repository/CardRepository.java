0531a6c400ca934f47b58304b76f4476b194556f
adamatolondon <adamato@antonios-mbp.lan>
2021-12-09 11:36:23 +0100

    added new method

--- src/main/java/api/card/repository/CardRepository.java
+++ src/main/java/api/card/repository/CardRepository.java
@@ -2,10 +2,13 @@ package api.card.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import api.card.model.Card;
 
 public interface CardRepository extends CrudRepository<Card, Long> {
 	List<Card> findByNumberAndCustomerId(String number, Long customerId);
+
+	List<Card> findByCustomerId(Long customerId, Sort sort);
 }
